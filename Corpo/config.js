cdc822d6920364c763c13fe3d50ef4bd962713bb
victorh1711 <victorhugosilva170711@gmail.com>
2025-06-22 11:46:32 -0300

    Finalizamos o projeto

---
+++ Corpo/config.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const area = document.getElementById('myMultiLineInput');
+
+  area.focus(); // Foca no textarea
+  area.setSelectionRange(area.value.length, area.value.length);
+
+  function insertAtCursor(myField, myValue) {t
+    if (document.selection) {
+      myField.focus();
+      sel = document.selection.createRange();
+      sel.text = myValue;
+    }
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+      var startPos = myField.selectionStart;
+      var endPos = myField.selectionEnd;
+      myField.value = myField.value.substring(0, startPos)
+        + myValue
+        + myField.value.substring(endPos, myField.value.length);
+      myField.selectionStart = startPos + myValue.length;
+      myField.selectionEnd = startPos + myValue.length;
+    } else {
+      myField.value += myValue;
+    }
+  }
+});
