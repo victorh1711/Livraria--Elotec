bea6ce0cfe1381d7037671255789b5166c1925ea
victorh1711 <victorhugosilva170711@gmail.com>
2025-09-09 22:05:22 -0300

    Finalização do projeto

---
+++ Script/meta.js
@@ -0,0 +1,33 @@
+const buttonForIndex = document.getElementById('forIndex')
+const buttonForConfig = document.getElementById('forConfig')
+const popUp = document.getElementById('popUp')
+const sendButton = document.getElementById('sendText')
+const backButton = document.getElementById('backButton')
+const textArea = document.getElementById('textArea')
+const mensagem = document.getElementById('mensagem')
+
+buttonForIndex.addEventListener("click", function(){
+    window.location.href = 'index.html'
+})
+  buttonForConfig.addEventListener("click", function(){
+    popUp.style.display = 'block'
+})
+backButton.addEventListener("click", function(){
+    popUp.style.display = 'none'
+    textArea.value = ''
+})
+sendButton.addEventListener("click", function(){
+    if(textArea.value.trim() == ''){
+        mensagem.style.color = 'red'
+        mensagem.textContent = 'O campo está vazio'
+        setTimeout(() => {
+            mensagem.textContent = ''
+        }, 2000);
+    }
+    else if(textArea.value.trim() != ''){
+        mensagem.style.color = 'blue' 
+        mensagem.textContent = 'Agradecemos seu feedback'
+        textArea.style.display = 'none'
+        sendButton.disabled = true
+    }
+})
